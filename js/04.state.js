c3ddb15a9a4585005dc4d90b251d9fe3a2277f1d
booldook <booldook@gmail.com>
2021-11-17 10:24:03 +0900

    04.state

--- js/04.state.js
+++ js/04.state.js
@@ -12,21 +12,40 @@ class List extends React.Component {
 
 class Title extends React.Component {
   render() {
-    return <h1 className="fa-2x my-2">자식컴포넌트 Title</h1>;
+    return <h1 className="fa-2x my-5">자식컴포넌트 Title</h1>;
   }
 }
 
 class Search extends React.Component {
+  onChangeInput = (e) => {
+    console.log(e.target.value);
+  };
   render() {
     return (
       <form>
-        <input type="text" className="form-control" name="query" autofocus />
+        <input
+          onChange={this.onChangeInput}
+          type="text"
+          className="form-control"
+          name="query"
+          autoFocus
+        />
       </form>
     );
   }
 }
 
 class App extends React.Component {
+  state = {
+    title: '',
+    lists: [],
+  };
+  changedTitle = (value) => {
+    this.setState({
+      ...this.state,
+      title: value,
+    });
+  };
   render() {
     return (
       <div className="container">
